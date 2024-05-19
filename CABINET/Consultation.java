958dbea142918a9bd988fd387b3a55792d8491cf
sarahhsira <165084386+sarahhsira@users.noreply.github.com>
2024-05-20 00:20:31 +0100

    Update Consultation.java

--- CABINET/Consultation.java
+++ CABINET/Consultation.java
@@ -1,118 +1,48 @@
 package Cabinet;
-import java.time.LocalDate; // Pour utiliser le type LocalDate pour représenter la date
-import java.time.LocalTime; // Pour utiliser le type LocalTime pour représenter l'heure
-
-public class Consultation {
-    private LocalDate date; // Date de la consultation
-    private LocalTime heure; // Heure de la consultation
-    private String motif; // Motif de la consultation
-    private String anamnese; // Anamnèse de la consultation
-    private String examenPhysique; // Examen physique de la consultation
-    private String diagnostic; // Diagnostic de la consultation
-    private Ordonnance ordonnance; // Ordonnance de la consultation
-    private CertificatMedical certificatMedical; // Certificat médical de la consultation
+public class Constantes {
+    private String tensionArterielle; // Tension artérielle du patient
+    private int frequenceCardiaque; // Fréquence cardiaque du patient
+    private float temperature; // Température corporelle du patient
 
     // Constructeur
-    public Consultation(LocalDate date, LocalTime heure, String motif, String anamnese, String examenPhysique, String diagnostic, Ordonnance ordonnance, CertificatMedical certificatMedical) {
-        this.date = date;
-        this.heure = heure;
-        this.motif = motif;
-        this.anamnese = anamnese;
-        this.examenPhysique = examenPhysique;
-        this.diagnostic = diagnostic;
-        this.ordonnance = ordonnance;
-        this.certificatMedical = certificatMedical;
+    public Constantes(String tensionArterielle, int frequenceCardiaque, float temperature) {
+        this.tensionArterielle = tensionArterielle;
+        this.frequenceCardiaque = frequenceCardiaque;
+        this.temperature = temperature;
     }
 
     // Getters et setters
-    public LocalDate getDate() {
-        return date;
+    public String getTensionArterielle() {
+        return tensionArterielle;
     }
 
-    public void setDate(LocalDate date) {
-        this.date = date;
+    public void setTensionArterielle(String tensionArterielle) {
+        this.tensionArterielle = tensionArterielle;
     }
 
-    public LocalTime getHeure() {
-        return heure;
+    public int getFrequenceCardiaque() {
+        return frequenceCardiaque;
     }
 
-    public void setHeure(LocalTime heure) {
-        this.heure = heure;
+    public void setFrequenceCardiaque(int frequenceCardiaque) {
+        this.frequenceCardiaque = frequenceCardiaque;
     }
 
-    public String getMotif() {
-        return motif;
+    public float getTemperature() {
+        return temperature;
     }
 
-    public void setMotif(String motif) {
-        this.motif = motif;
+    public void setTemperature(float temperature) {
+        this.temperature = temperature;
     }
 
-    public String getAnamnese() {
-        return anamnese;
-    }
-
-    public void setAnamnese(String anamnese) {
-        this.anamnese = anamnese;
-    }
-
-    public String getExamenPhysique() {
-        return examenPhysique;
-    }
-
-    public void setExamenPhysique(String examenPhysique) {
-        this.examenPhysique = examenPhysique;
-    }
-
-    public String getDiagnostic() {
-        return diagnostic;
-    }
-
-    public void setDiagnostic(String diagnostic) {
-        this.diagnostic = diagnostic;
-    }
-
-    public Ordonnance getOrdonnance() {
-        return ordonnance;
-    }
-
-    public void setOrdonnance(Ordonnance ordonnance) {
-        this.ordonnance = ordonnance;
-    }
-
-    public CertificatMedical getCertificatMedical() {
-        return certificatMedical;
-    }
-
-    public void setCertificatMedical(CertificatMedical certificatMedical) {
-        this.certificatMedical = certificatMedical;
-    }
-
-    // Méthode toString() pour afficher les informations de la consultation
+    // Méthode toString() pour afficher les informations des constantes médicales
     @Override
     public String toString() {
-        return "Consultation{" +
-                "date=" + date +
-                ", heure=" + heure +
-                ", motif='" + motif + '\'' +
-                ", anamnese='" + anamnese + '\'' +
-                ", examenPhysique='" + examenPhysique + '\'' +
-                ", diagnostic='" + diagnostic + '\'' +
-                ", ordonnance=" + ordonnance +
-                ", certificatMedical=" + certificatMedical +
+        return "Constantes{" +
+                "tensionArterielle='" + tensionArterielle + '\'' +
+                ", frequenceCardiaque=" + frequenceCardiaque +
+                ", temperature=" + temperature +
                 '}';
     }
-    // Méthode pour ajouter une ordonnance à la consultation
-
-    public void ajouterOrdonnance(Ordonnance ordonnance) {
-        this.ordonnance = ordonnance;
-    }
-// Méthode pour ajouter un certificat médical à la consultation
-
-public void ajouterCertificatMedical(CertificatMedical certificatMedical) {
-    this.certificatMedical = certificatMedical;
-}
-
-    
 }
